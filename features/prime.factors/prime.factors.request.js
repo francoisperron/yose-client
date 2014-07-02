ca7f2d2aa0c2d972598930d443da8dafa5339c9b
Francois Perron <fperron@gmail.com>
2014-07-01 23:48:56 -0400

    displays multiple results

--- features/prime.factors/prime.factors.request.js
+++ features/prime.factors/prime.factors.request.js
@@ -5,16 +5,46 @@ function PrimeFactorsRequest() {
 
 PrimeFactorsRequest.prototype.decompose = function (numbers) {
     var queryString = '?number=' + numbers.split(',').join('&number=');
-    $.get('/primeFactors' + queryString, displayResult);
+    $.get('/primeFactors' + queryString).success(displayResult);
 };
 
 function displayResult(data) {
+    $('#result').empty();
+    $('#results').empty();
+
+    if (data instanceof Array) {
+        displayMultipleResults(data);
+    }
+    else {
+        displayOneResult(data);
+    }
+}
+
+function displayMultipleResults(datas) {
+    $('#result-title').html('Result');
+
+    for (var i = 0; i < datas.length; i++) {
+        var resultItem;
+        var data = datas[i];
+        if (data.error == 'not a number') {
+            resultItem = data.number + ' is ' + data.error;
+        }
+        else if (data.error) {
+            resultItem = data.error;
+        }
+        else {
+            resultItem = data.number + ' = ' + data.decomposition.join(' x ');
+        }
+        $('#results').append('<li>' + resultItem + '</li>')
+    }
+}
+function displayOneResult(data) {
     $('#result-title').html('Result');
 
     if (data.error == 'not a number') {
         $('#result').html(data.number + ' is ' + data.error);
     }
-    else if(data.error){
+    else if (data.error) {
         $('#result').html(data.error);
     }
     else {
--- features/prime.factors/prime.factors.request.spec.js
+++ features/prime.factors/prime.factors.request.spec.js
@@ -6,17 +6,55 @@ describe('Prime factors request', function () {
 
     var request = new PrimeFactorsRequest();
 
+    beforeEach(function () {
+        $('body').append('<div id="result-title"/><div id="result"/><ol id="results"/>');
+    });
+
     it('calls primeFactors endpoint with the number to decompose', function () {
-        spyOn($, 'get');
+        spyOn($, 'get').andCallThrough();
         request.decompose('25');
 
-        expect($.get).toHaveBeenCalledWith('/primeFactors?number=25', jasmine.any(Function));
+        expect($.get).toHaveBeenCalledWith('/primeFactors?number=25');
     });
 
     it('calls primeFactors endpoint with multiple numbers to decompose', function () {
-        spyOn($, 'get');
+        spyOn($, 'get').andCallThrough();
         request.decompose('25,43,1');
 
-        expect($.get).toHaveBeenCalledWith('/primeFactors?number=25&number=43&number=1', jasmine.any(Function));
+        expect($.get).toHaveBeenCalledWith('/primeFactors?number=25&number=43&number=1');
+    });
+
+    it('displays single result in #result', function () {
+        spyOn($, 'get').andCallFake(function () {
+            return {
+                success: function (callback) {
+                    callback({number: 25, decomposition: [5, 5]});
+                }
+            };
+        });
+
+        request.decompose('25');
+
+        expect($('#result').html()).toEqual('25 = 5 x 5')
+    });
+
+    it('displays multiple results in #results', function () {
+        spyOn($, 'get').andCallFake(function () {
+            return {
+                success: function (callback) {
+                    callback([
+                        {number: 14, decomposition: [2, 7]},
+                        {number: 'non', error: 'not a number'},
+                        {number: 1234567, error: 'erreur de gros'}
+                    ]);
+                }
+            };
+        });
+
+        request.decompose('14,non,1234567');
+
+        expect($('ol#results li:nth-child(1)').html()).toEqual('14 = 2 x 7');
+        expect($('ol#results li:nth-child(2)').html()).toEqual('non is not a number');
+        expect($('ol#results li:nth-child(3)').html()).toEqual('erreur de gros');
     });
 });
