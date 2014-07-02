4f8050570f55d30613e700fe3abdd241e1450854
Francois Perron <fperron@gmail.com>
2014-07-01 22:40:38 -0400

    supports multiple numbers in input

--- features/prime.factors/prime.factors.request.js
+++ features/prime.factors/prime.factors.request.js
@@ -3,22 +3,21 @@ var $ = $ || require('jquery');
 function PrimeFactorsRequest() {
 }
 
-PrimeFactorsRequest.prototype.decompose = function (number) {
-    if(isNaN(number)){
-        displayResult({error: number + ' is not a number'});
-        return;
-    }
-
-    $.get('/primeFactors', { number: number }, displayResult);
+PrimeFactorsRequest.prototype.decompose = function (numbers) {
+    var queryString = '?number=' + numbers.split(',').join('&number=');
+    $.get('/primeFactors' + queryString, displayResult);
 };
 
 function displayResult(data) {
     $('#result-title').html('Result');
 
-    if(data.error){
+    if (data.error == 'not a number') {
+        $('#result').html(data.number + ' is ' + data.error);
+    }
+    else if(data.error){
         $('#result').html(data.error);
     }
-    else{
+    else {
         $('#result').html(data.number + ' = ' + data.decomposition.join(' x '));
     }
 }
--- features/prime.factors/prime.factors.request.spec.js
+++ features/prime.factors/prime.factors.request.spec.js
@@ -6,10 +6,17 @@ describe('Prime factors request', function () {
 
     var request = new PrimeFactorsRequest();
 
-    it('calls primeFactors endpoint', function () {
+    it('calls primeFactors endpoint with the number to decompose', function () {
         spyOn($, 'get');
-        request.decompose(25);
+        request.decompose('25');
 
-        expect($.get).toHaveBeenCalledWith('/primeFactors', {number: 25}, jasmine.any(Function));
+        expect($.get).toHaveBeenCalledWith('/primeFactors?number=25', jasmine.any(Function));
+    });
+
+    it('calls primeFactors endpoint with multiple numbers to decompose', function () {
+        spyOn($, 'get');
+        request.decompose('25,43,1');
+
+        expect($.get).toHaveBeenCalledWith('/primeFactors?number=25&number=43&number=1', jasmine.any(Function));
     });
 });
